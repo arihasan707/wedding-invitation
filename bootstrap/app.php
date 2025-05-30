db288ce8ebfac3fab950e6ef22f226ef26725fde
LAPTOP-R0HO03P8\daaru <arihasan707@gmail.com>
2025-05-31 04:36:35 +0700

    add cors v4

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,9 @@
 <?php
 
 use Illuminate\Foundation\Application;
+use App\Http\Middleware\CorsMiddleware;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'cors' => HandleCors::class
+            'cors' => CorsMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {

a8feed5dd98f70a5df78863b0e8afefb55d7f415
LAPTOP-R0HO03P8\daaru <arihasan707@gmail.com>
2025-05-31 09:39:02 +0700

    add cors v6

--- config/cors.php
+++ config/cors.php
@@ -2,20 +2,26 @@
 
 return [
 
+    /*
+|-------------------------------------------------------------------
+| Cross-Origin Resource Sharing (CORS) Configuration
+|-------------------------------------------------------------------
+|
+| Here you may configure your settings for cross-origin resource sharing
+| or "CORS". This determines what cross-origin operations may execute
+| in web browsers. You are free to adjust these settings as needed.
+|
+| To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+|
+*/
+
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['https://annisaari-theanswerisari.my.id'], // ← bisa diganti dengan domain tertentu di production
-
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,6 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-Route::get('commentar', [CommentarController::class, 'index']);
-Route::get('commentar/load', [CommentarController::class, 'loadMore']);
-Route::post('commentar/add', [CommentarController::class, 'store']);
+Route::get('/commentar', [CommentarController::class, 'index']);
+Route::get('/commentar/load', [CommentarController::class, 'loadMore']);
+Route::post('/commentar/add', [CommentarController::class, 'store']);
